b84bfc4a4b589029dd36bfa93eb43fc627e70f3a
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2024-11-18 03:46:37 +0000

    Update Enemy.java
    changing top comment

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,9 +1,9 @@
 /**
- * <p>This Abstract Enemy class extends Entity and is intended to hold 2 abstract functions to detect if the player has
- * touched the enemy and what to do.</p>
+ * <p>This Abstract Enemy class extends Entity and is intended to hold 3 abstract functions to detect if the player has
+ * touched the enemy, what to do and its future moves.</p>
  *
  * @author Joseph Parish.
- * @version 1.0.1
+ * @version 1.0.2
  * Last Changed: 18/11/24
  */
 public abstract class Enemy extends Entity
