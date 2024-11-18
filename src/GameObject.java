dc87690952b5933300cb4d19680d61f716360612
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2024-11-18 03:16:36 +0000

    Update GameObject.java
    Cent on crc cards at first, added functions and variables that were on the class diagrams.

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,20 +1,21 @@
 /**
- * <p>This Abstract GameObject class is intended to hold information related to the Sprite, the update and collision
- * check rate as well as a abstract update function.</p>
+ * <p>This Abstract GameObject class is intended to hold information related to the Sprite, the update rate and collision
+ * check result as well as an abstract Update, Delete and 2 Collision functions.</p>
  *
  * @author Joseph Parish.
- * @version 1.0.0
+ * @version 1.0.1
  * Last Changed: 18/11/24
  */
 public abstract class GameObject
 {
     private String pathToSprite; // Sprite
-
     private int[] centerSprite; // Center of Sprite
-
-    public abstract void update(); // Updates itself
-
+    private int[] position; // Position
+    private boolean isColliding; // Collision
     private int updateRate; // Update rate
 
-    private int collisionRate;// Collision Check rate
+    public abstract void update(); // Updates itself
+    public abstract boolean collisionCheck(); // Checks for collision 
+    public abstract void onCollision(); // Reaction to collision 
+    public abstract void delete(); // Destroys game object
 }
