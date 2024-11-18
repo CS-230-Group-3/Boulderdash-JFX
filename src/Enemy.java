001626e6d488d4d04b08eac26dc76b3ee83b6210
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2024-11-18 03:45:29 +0000

    Update Enemy.java
    realised that findpath() can be made abstract and implemented in Pathfindingenemy and Patrolingenemy

--- src/Enemy.java
+++ src/Enemy.java
@@ -1,15 +1,16 @@
 /**
- * <p>This Abstract Enemy class extends Entity and is intended to hold information related to its living state
- * as well as 2 abstract functions to detect if the player has touched the enemy and what to do.</p>
+ * <p>This Abstract Enemy class extends Entity and is intended to hold 2 abstract functions to detect if the player has
+ * touched the enemy and what to do.</p>
  *
  * @author Joseph Parish.
- * @version 1.0.0
+ * @version 1.0.1
  * Last Changed: 18/11/24
  */
 public abstract class Enemy extends Entity
 {
-    private boolean livingState; // Living state (!! I think this makes more sense to be in Entity as its in player too)
-
+    private int[][] path; // Path of future movement
+    
+    public abstract int[][] findPath(int[][] map, int[] enemyPos, int[] playerPos); // Finds path
     public abstract void onPlayerCollision(); // Reacts to players touch
     public abstract boolean playerCollisionCheck(boolean Collision); // Checks if touching player
 }
