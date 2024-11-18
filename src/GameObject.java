d089eefc05da6e7a31deb9af5815d5bcc7896936
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2024-11-18 03:08:12 +0000

    Update GameObject.java

--- src/GameObject.java
+++ src/GameObject.java
@@ -1,2 +1,20 @@
-public abstract class GameObject {
+/**
+ * <p>This Abstract GameObject class is intended to hold information related to the Sprite, the update and collision
+ * check rate as well as a abstract update function.</p>
+ *
+ * @author Joseph Parish.
+ * @version 1.0.0
+ * Last Changed: 18/11/24
+ */
+public abstract class GameObject
+{
+    private String pathToSprite; // Sprite
+
+    private int[] centerSprite; // Center of Sprite
+
+    public abstract void update(); // Updates itself
+
+    private int updateRate; // Update rate
+
+    private int collisionRate;// Collision Check rate
 }
