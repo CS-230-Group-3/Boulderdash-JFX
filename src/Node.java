b5447b25a2f9f3c017186ba211210d177f4c9a1a
PayyC <dikovspas@gmail.com>
2024-11-18 07:25:42 +0000

    Stub class Node added to complete PathfindingEnemy

---
+++ src/Node.java
@@ -0,0 +1,22 @@
+//Node class to complete PathfindingEnemy
+// NOT FINAL
+public class Node {
+    public int x;
+    public int y;
+    public int g;
+    public int h;
+    public int f;
+    public Node parent;
+
+    public Node(int x, int y, Node parent) {
+        this.x = x;
+        this.y = y;
+        this.parent = parent;
+    }
+    public Node(int x, int y, int g, int h, Node parent) {
+        this(x, y, parent);
+        this.g = g;
+        this.h = h;
+    }
+
+}
