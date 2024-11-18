79052411e8ead9d0c1638efe2d26afc325cb5689
PayyC <dikovspas@gmail.com>
2024-11-18 07:24:07 +0000

    Stub method implementation for Player

--- src/Player.java
+++ src/Player.java
@@ -1,2 +1,21 @@
 public class Player extends Entity{
+    public void move() {
+
+    }
+
+    public void update() {
+
+    }
+
+    public boolean collisionCheck() {
+        return false;
+    }
+
+    public void onCollision() {
+
+    }
+
+    public void delete() {
+        
+    }
 }
