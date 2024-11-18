be8681e6677addbd8670aa17f30d360eaaf85666
Joseph Parish <113930162+JosephParish@users.noreply.github.com>
2024-11-18 03:47:41 +0000

    Update Entity.java

--- src/Entity.java
+++ src/Entity.java
@@ -1,2 +1,15 @@
-public abstract class Entity extends GameObject{
+/**
+ * <p>This Abstract Entity class extends GameObject and is intended to hold information related to its living state
+ * as well as hold an abstract movement method.</p>
+ *
+ * @author Joseph Parish.
+ * @version 1.0.0
+ * Last Changed: 18/11/24
+ */
+public abstract class Entity extends GameObject
+{
+
+    private boolean livingState; // Living state
+
+    public abstract void move(); // Movement
 }
