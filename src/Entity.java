74230e85746f3b71f8891c8767e7f20a54dff084
Oscar Baggs <oscarbaggs@gmail.com>
2024-11-23 12:07:05 +0000

    Updated Entity, GameObject and Tile

--- src/Entity.java
+++ src/Entity.java
@@ -1,15 +1,19 @@
 /**
- * <p>This Abstract Entity class extends GameObject and is intended to hold information related to its living state
- * as well as hold an abstract movement method.</p>
+ * Abstract Entity class derived from abstract GameObject class. Includes new abstract move method.
  *
- * @author Joseph Parish.
- * @version 1.0.0
- * Last Changed: 18/11/24
+ * @author Oscar and Ahmed.
+ * @version 1.0.2
+ * Last changed: 22/11/2024
  */
-public abstract class Entity extends GameObject
-{
 
-    private boolean livingState; // Living state
+public abstract class Entity extends GameObject {
 
-    public abstract void move(); // Movement
+    public Entity(Sprite sprite, int[] position, int[] centreOfSprite, int updateRate) {
+        super(sprite, position, centreOfSprite, updateRate);
+    }
+
+    /**
+     * Changes the entity's position.
+     */
+    public abstract void move();
 }
--- src/Tile.java
+++ src/Tile.java
@@ -1,2 +1,34 @@
-public abstract class Tile extends GameObject{
+/**
+ * Abstract Tile class derived from abstract GameObject class. Includes new walkable and destroyable
+ * attributes as well as getters.
+ *
+ * @author Oscar and Ahmed.
+ * @version 1.0.2
+ * Last changed: 22/11/2024
+ */
+
+public abstract class Tile extends GameObject {
+
+    private boolean walkable;
+    private boolean destroyable;
+
+    public Tile(Sprite sprite, int[] position, int[] centreOfSprite, int updateRate) {
+        super(sprite, position, centreOfSprite, updateRate);
+    }
+
+    /**
+     * Checks if the tile is walkable.
+     * @return true if the tile is walkable, false if not.
+     */
+    protected boolean isWalkable() {
+        return this.walkable;
+    }
+
+    /**
+     * Checks if the tile is destroyable.
+     * @return true if the tile is destroyable, false if not.
+     */
+    protected boolean isDestroyable() {
+        return this.destroyable;
+    }
 }
