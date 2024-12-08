7c019812f6fef6c33dc0da4078047712f4191aec
YuliiaShubina <shubinayuliiasergiivna@gmail.com>
2024-12-08 21:41:45 +0000

    new changes to ui new button continue

--- src/MainUI.java
+++ src/MainUI.java
@@ -8,8 +8,8 @@ import javafx.fxml.FXMLLoader;
 import java.io.IOException;
 
 public class MainUI extends Application {
-	public static final int MAIN_WINDOW_WIDTH = 1920;
-	public static final int MAIN_WINDOW_HEIGHT = 1080;
+	public static final int MAIN_WINDOW_WIDTH = 1400;
+	public static final int MAIN_WINDOW_HEIGHT = 800;
 
 	@Override
 	public void start(Stage stage) throws IOException {
