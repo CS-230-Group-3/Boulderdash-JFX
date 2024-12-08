63a0e301ac45cdc18d119232a4490f3d396095ed
Oscar <oscarbaggs@gmail.com>
2024-12-08 20:17:07 +0000

    Water and magicwall logic for boulder and gem

--- src/Gem.java
+++ src/Gem.java
@@ -1,22 +1,32 @@
 public class Gem extends Item {
 
     private static final String FILE_PATH = "resources/assets/diamondblue.png";
+    private boolean waterSkip = true;
 
     public Gem() {
         super(FILE_PATH, new GridPosition(0, 0));
-        this.updateRate = 1;
+        this.updateRate = 3;
         this.type = "gem";
     }
 
     public void update(Map map) {
+        if (map.getTileAt(this.getPosition()) instanceof Water && !waterSkip) {
+            waterSkip = true;
+        } else if (map.getTileAt(this.getPosition()) instanceof Water && waterSkip) {
+            waterSkip = false;
+            return;
+        }
         GameObject downNeighbour = map.getNeighbourOf(this, Direction.DOWN);
 
         if (!this.fall(map)) {
             this.roll(map);
         }
         if (downNeighbour instanceof MagicWall) {
-            this.delete();
-            //spawn boulder under magic wall
+            map.getPendingRemovals().add(this);
+            GameObject belowMagicWall = map.getNeighbourOf(downNeighbour, Direction.DOWN);
+            Boulder boulder = new Boulder();
+            boulder.setPosition(belowMagicWall.getPosition());
+            map.getPendingAdditions().add(boulder);
         }
     }
     public void delete() {
